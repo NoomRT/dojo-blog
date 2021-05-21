1b68d3dad7896d60e41e0e5398610760f2a8166a
NoomRT <rueangyotnoom@gmail..com>
2021-05-21 23:11:09 +0700

    Lesson 5: Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+   const handleClick = (e) => {
+      console.log('hello, Noom', e);
+   };
+
+   const handleClickAgain = (name, e) => {
+      console.log('hello ' + name, e.target);
+   };
+
     return ( 
         <div className="home">
            <h2>Homepage</h2> 
+           {/* use function reference to invoke the function each times*/}
+           <button onClick={handleClick}>Click me</button>
+           {/* use anonymous function to invoke function that have parameter */}
+           <button onClick={(e) => handleClickAgain('Noom', e)}>Click me again</button>
+           {/* access to the event object or the event parameter by input e in parameter */}
         </div>
      );
 }
