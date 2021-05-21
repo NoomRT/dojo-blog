0c6809c30991d30db7753e0f31f082097ebf422a
NoomRT <rueangyotnoom@gmail..com>
2021-05-21 23:23:35 +0700

    Lesson 6: Using Hooks ( useState hook )

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+   // let name = 'Noom';
+   const [name, setName] = useState('Noom');
+   const [age, setAge] = useState(25);
 
-   const handleClick = (e) => {
-      console.log('hello, Noom', e);
-   };
-
-   const handleClickAgain = (name, e) => {
-      console.log('hello ' + name, e.target);
+   const handleClick = () => {
+      setName('Nim');
+      setAge(30);
    };
 
     return ( 
         <div className="home">
            <h2>Homepage</h2> 
-           {/* use function reference to invoke the function each times*/}
+           <p>{ name } is { age } years old</p>
            <button onClick={handleClick}>Click me</button>
-           {/* use anonymous function to invoke function that have parameter */}
-           <button onClick={(e) => handleClickAgain('Noom', e)}>Click me again</button>
-           {/* access to the event object or the event parameter by input e in parameter */}
         </div>
      );
 }
